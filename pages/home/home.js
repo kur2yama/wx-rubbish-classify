ae8ccae14dc0e5c1351f402b93c4a944d60b5c6c
wangkang <794372769@qq.com>
2019-07-05 14:01:59 +0800

    7-5

---
+++ pages/home/home.js
@@ -0,0 +1,76 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navs: ["湿垃圾", "干垃圾", "可回收物", "有害垃圾"],
+    current: 0
+  },
+  changeCurrent(e) {
+    this.setData({
+      current: e.target.dataset.idx
+    })
+  },
+  swiperchange(e) {
+    this.setData({
+      current: e.detail.current
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
---
+++ pages/item/item.js
@@ -0,0 +1,83 @@
+// pages/item/item.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    itemList:[
+      {
+        itemTitle:"小龙虾",
+        src:"../../images/IMG_2047_03.png"
+      },
+      {
+        itemTitle: "粽子",
+        src: "../../images/IMG_2046_03.png"
+      },
+      {
+        itemTitle: "奶茶",
+        src: "../../images/IMG_2048_11.png"
+      },
+      {
+        itemTitle: "电池",
+        src: "../../images/IMG_2048_13.png"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
